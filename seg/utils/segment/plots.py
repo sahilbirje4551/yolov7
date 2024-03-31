0dd216a6a7a03e17c96840a1ba3e6a662dc3c971
sahilbirje4551 <78172846+sahilbirje4551@users.noreply.github.com>
2024-04-01 01:01:29 +0530

    Update plots.py
    numpy deprecated fix for plots.py

--- seg/utils/segment/plots.py
+++ seg/utils/segment/plots.py
@@ -137,9 +137,9 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
                         if mh != h or mw != w:
                             mask = image_masks[j].astype(np.uint8)
                             mask = cv2.resize(mask, (w, h))
-                            mask = mask.astype(np.bool)
+                            mask = mask.astype(np.bool_)
                         else:
-                            mask = image_masks[j].astype(np.bool)
+                            mask = image_masks[j].astype(np.bool_)
                         with contextlib.suppress(Exception):
                             im[y:y + h, x:x + w, :][mask] = im[y:y + h, x:x + w, :][mask] * 0.4 + np.array(color) * 0.6
                 annotator.fromarray(im)
